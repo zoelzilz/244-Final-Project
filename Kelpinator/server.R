5191c476f51db60076a1bbf19b1f82176355bad6
Zoe Zilz <zilz@ucsb.edu>
2019-02-25 13:51:31 -0800

    added unfinished code for structuring tabs 1 and 2, none of it works because data isn't ready to plug in

--- Kelpinator/server.R
+++ Kelpinator/server.R
@@ -5,11 +5,25 @@
 # called once each session
 
 server <- function(input,output, session){
+  # map output for first tab (there are so many ways to do this, WIP)
+  output$mymap <- renderLeaflet({
+    m <- leaflet() %>%
+      addTiles() %>%
+      setView(lng=-119.6247004, lat=33.6884588 , zoom=8.06)
+    m
+    
+    
+  })
+  
+  # bar chart output for second tab
+  output$barchart <- renderPlot({
+    if ( input$day == "Both" ) {
+      journeyTimeHist.f(combined)
+    } else if ( input$day == "Weekend") {
+      journeyTimeHist.f(combined, "Weekend")
+    } else if ( input$day == "Weekday") {
+      journeyTimeHist.f(combined, "Weekday")
+    }
+  })
 }
 
-output$mymap <- renderLeaflet({
-  m <- leaflet() %>%
-    addTiles() %>%
-    setView(lng=-119.6247004, lat=33.6884588 , zoom=8.06)
-  m
-})
